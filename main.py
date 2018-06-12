a822aa1f57bd822a155006008c590fe93ba3c16a
pandasumm <pandasumm@gmail.com>
2018-06-13 04:46:06 +0800

    added MLP algorithm

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 # from sklearn.ensemble import RandomForestClassifier
 # from sklearn.linear_model import LogisticRegression
 
 
 def dataCleaning(dataset):
     dataset.title = LabelEncoder().fit_transform(dataset.title)
+    dataset.developer = dataset.developer.fillna('N')
     dataset.developer = LabelEncoder().fit_transform(dataset.developer)
     dataset.rating = dataset.rating.fillna('E')
     dataset.rating = LabelEncoder().fit_transform(dataset.rating)
@@ -42,18 +44,29 @@ def dataCleaning(dataset):
 
 if __name__ == "__main__":
 
-    dataset = pd.read_csv("./games_added_publisher.csv")
-    dataset.at[dataset['user_score'].abs() <= 5, 'user_score'] = 0
-    dataset.at[dataset['user_score'].abs() > 5, 'user_score'] = 1
+    dataset = pd.read_csv("./games_521.csv")
+    dataset.at[dataset['user_score'].between(0,3,inclusive=False), 'user_score'] = 0
+    dataset.at[dataset['user_score'].between(3,5,inclusive=False), 'user_score'] = 1
+    dataset.at[dataset['user_score'].between(5,7,inclusive=False), 'user_score'] = 2
+    dataset.at[dataset['user_score'].between(7,9,inclusive=False), 'user_score'] = 3
+    dataset.at[dataset['user_score'].between(9,10), 'user_score'] = 3
     resY = dataset.user_score
-    dataset.drop(['user_score'], axis = 1)
-    
+    dataset = dataset.drop(['user_score'], axis = 1)
+
     dataX = dataCleaning(dataset)
 
     dataX.to_csv('./temp.csv', index=False)
 
+    # 15-NN classifier
     classifier = KNeighborsClassifier(n_neighbors = 15, metric = 'minkowski', p = 2)
-
     classifier.fit(dataX, resY)
     accuracies = cross_val_score(estimator = classifier, X = dataX, y= resY, cv = 10)
-    print("accuracies: ", accuracies.mean(), "+/-", accuracies.std(),"\n")
+    print("15-nn classifier accuracies: ", accuracies.mean(), "+/-", accuracies.std(),"\n")
+
+
+    # MLPClassifier, multi layer perceptron
+    # classifier = KNeighborsClassifier(n_neighbors = 15, metric = 'minkowski', p = 2)
+    classifier = MLPClassifier(solver='lbfgs', alpha = 1e-5, hidden_layer_sizes=(5,2), random_state=1)
+    classifier.fit(dataX, resY)
+    accuracies = cross_val_score(estimator = classifier, X = dataX, y= resY, cv = 10)
+    print("multi layer perceptron accuracies: ", accuracies.mean(), "+/-", accuracies.std(),"\n")
